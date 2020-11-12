9fa527071748aa17f432726d679a873d8c3e8bd8
DerSkillBaumApfel <DerSkillBaumApfel@users.noreply.github.com>
2020-11-12 13:17:51 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul')
-var fs = require('fs');
 
 gulp.task('default', function () {
     gulp.src('tests/**/*.js')
